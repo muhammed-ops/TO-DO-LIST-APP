0e522443c6eacadca88726200687c860460d7e18
muhammed <adekolamuhammed89@gmail.com>
2023-04-23 15:48:24 +0100

    todo list

---
+++ todo.js
@@ -0,0 +1,20 @@
+let arr = [];
+        function Click(){
+        let a = document.getElementById('change').value;
+        if(a!==''){
+        arr.push(a);
+        document.getElementById('change').value = '';
+        console.log(arr);
+        document.getElementById('list').innerHTML =  arr.map((i,inde,arr) => `<ul><li>${i}</li></ul>`).join('');
+        }}
+        ;
+
+        function Clear(){
+            arr=[];
+            document.getElementById('list').innerHTML =  arr.map(i =>  `i`);
+        };
+        function Delete(arr){
+           var a = arr.filter((arr,inde)=>{inde !== inde})
+           console.log(a)
+            
+        }
